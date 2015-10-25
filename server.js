156f75a41bc8127513308dbeb41011046391b166
xuyuanme <xuyuanme@gmail.com>
2015-10-25 12:33:52 +0800

    Reformat code

--- server.js
+++ server.js
@@ -24,23 +24,23 @@ var _tApiProfile = "https://api.twitter.com/1.1/account/verify_credentials.json"
 
 var port = process.env.PORT || 8483        // set our port
 var app = express()                 // define our app using express
-app.use(session({ secret: "React Twitter Secret" }))
+app.use(session({secret: "React Twitter Secret"}))
 
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router()              // get an instance of the express Router
 
-router.get('/requestToken', function(req, res) {
+router.get('/requestToken', function (req, res) {
     console.log("Get /twitter/requestToken")
 
-    if(isAppClient(req)) {
+    if (isAppClient(req)) {
         consumer._authorize_callback = _requestTokenAppCallBackUrl
     } else {
         consumer._authorize_callback = _requestTokenWebCallBackUrl
     }
 
     consumer.getOAuthRequestToken(
-        function(error, oauthToken, oauthTokenSecret, results){
+        function (error, oauthToken, oauthTokenSecret, results) {
             if (error) {
                 handleError(req, res, error)
             } else {
@@ -49,8 +49,8 @@ router.get('/requestToken', function(req, res) {
                 req.session.oauthRequestTokenSecret = oauthTokenSecret
                 console.log(req.session)
 
-                if(isAppClient(req)) {
-                    res.json({ "oauthRequestToken" : req.session.oauthRequestToken })
+                if (isAppClient(req)) {
+                    res.json({"oauthRequestToken": req.session.oauthRequestToken})
                 } else {
                     res.redirect(_tOauthAuthorize + req.session.oauthRequestToken)
                 }
@@ -61,10 +61,10 @@ router.get('/requestToken', function(req, res) {
     )
 })
 
-router.get('/accessToken', function(req, res) {
+router.get('/accessToken', function (req, res) {
     console.log("Get /twitter/accessToken")
-    consumer.getOAuthAccessToken( req.session.oauthRequestToken, req.session.oauthRequestTokenSecret, req.query.oauth_verifier,
-        function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
+    consumer.getOAuthAccessToken(req.session.oauthRequestToken, req.session.oauthRequestTokenSecret, req.query.oauth_verifier,
+        function (error, oauthAccessToken, oauthAccessTokenSecret, results) {
             if (error) {
                 console.log("Error results: " + util.inspect(results))
                 handleError(req, res, error)
@@ -74,7 +74,7 @@ router.get('/accessToken', function(req, res) {
                 req.session.oauthAccessTokenSecret = oauthAccessTokenSecret
                 console.log(req.session)
 
-                if(isAppClient(req)) {
+                if (isAppClient(req)) {
                     res.send('OK')
                 } else {
                     res.redirect('/twitter/profile')
@@ -86,9 +86,9 @@ router.get('/accessToken', function(req, res) {
     )
 })
 
-router.get('/profile', function(req, res) {
+router.get('/profile', function (req, res) {
     console.log("Get /twitter/profile")
-    consumer.get( _tApiProfile, req.session.oauthAccessToken, req.session.oauthAccessTokenSecret,
+    consumer.get(_tApiProfile, req.session.oauthAccessToken, req.session.oauthAccessTokenSecret,
         function (error, data, response) {
             if (error) {
                 handleError(req, res, error)
@@ -100,12 +100,12 @@ router.get('/profile', function(req, res) {
         })
 })
 
-router.get('*', function(req, res) {
+router.get('*', function (req, res) {
     res.redirect('/twitter/profile')
 })
 
 function isAppClient(req) {
-    if(req.headers['user-agent'].substr(0,12)==='ReactTwitter') {
+    if (req.headers['user-agent'].substr(0, 12) === 'ReactTwitter') {
         return true
     } else {
         return false
@@ -114,9 +114,9 @@ function isAppClient(req) {
 
 function handleError(req, res, error) {
     console.log("Error: " + util.inspect(error))
-    if(isAppClient(req)) {
+    if (isAppClient(req)) {
         res.status(error.statusCode).send(util.inspect(error))
-    } else if(error.statusCode === 403) {
+    } else if (error.statusCode === 403) {
         res.redirect('/twitter/requestToken')
     }
 }
